7a28954968ae978bf0bc3970ec71368813acb680
Jagadeeswar J <115331104+JagadeeswarJ@users.noreply.github.com>
2025-03-24 10:54:14 +0000

    basic chat broadcast

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const http = require("http");
 const exp = require("express");
 const app = exp();
-
+const path = require("path");
+const { Server } = require("socket.io");
 const server = http.createServer(app);
-// app.listen(3000, () => {
-//   console.log("Server is running on port 3000");
-// });
+const io = new Server(server);
 
+// socket.io setup
+io.on("connection", (socket) => {
+  console.log("A user connected",socket.id);
+  socket.on("message", (msg) => {
+    console.log("message: " + msg);
+    io.emit("received", msg);
+  });
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+ 
+app.use(exp.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
-  res.send("Hello World");
-} );
-
+  res.send("Hello World"); 
+});
 
 server.listen(9000, () => {
   console.log("Server is running on port 9000");
